16890fbcde023b7bb0b0bbb13e623c4a05ead5d4
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-24 08:16:40 +0530

    adding file stream

---
+++ Streams/FileStream.exs
@@ -0,0 +1,7 @@
+Stream.resource(fn -> File.open!("sample") end,
+                fn file ->
+                  case IO.read(file, :line) do
+                    data when is_binary(data) -> {[data], file} _ -> {:halt, file}
+                  end
+                end,
+                fn file -> File.close(file) end)
