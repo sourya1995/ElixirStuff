8464fb10a948a3b8ec072baecee4cd2dc5564e48
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-25 13:09:41 +0530

    Adding Logging

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -1,5 +1,7 @@
 defmodule Issues.CLI do
 
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
+
   @default_count 4
 
   @moduledoc """
@@ -8,7 +10,7 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
 
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
@@ -53,6 +55,7 @@ defmodule Issues.CLI do
     |> decode_response()
     |> sort_into_descending_order()
     |> last(count)
+    |> print_table_for_columns(["number", "created_At", "title"])
   end
 
   def last(list, count) do
--- issues/mix.exs
+++ issues/mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config()
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
@@ -24,7 +25,14 @@ defmodule Issues.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       { :httpoison, "~> 1.0.0" },
-      { :poison, "~> 3.1" }, 
+      { :poison, "~> 3.1" },
+      { :ex_doc, "~> 0.18.1" },
+      { :earmark, "~> 1.2.4"}
     ]
   end
+
+  defp escript_config do
+    [
+      main_module: Issues.CLI
+    ]
 end
